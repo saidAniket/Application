7abd68a443294fd80b82904167e632bc98bb2c44
Aniket <119159622+saidAniket@users.noreply.github.com>
2023-03-09 22:21:38 +0530

    Add files via upload

---
+++ program111.c
@@ -0,0 +1,62 @@
+/*
+    rows   4
+    col    4
+
+   @    2   3   4
+   1    @   3   4
+   1    2   @   4
+   1    2   3   @
+
+
+*/ 
+#include<stdio.h>
+void Display(int iRow,int iCol)
+{
+    int i=0;
+    int j=0;
+
+    if(iRow < 0)
+    {
+        iRow= -iRow;
+    }
+    if(iCol < 0)
+    {
+        iCol= -iCol;
+    }
+    if(iRow !=iCol) // jar row ani column vegvegle dile tar
+    {
+        printf("ROW number and column number are different\n");
+        return;
+    }
+
+    for(i =1; i <= iRow; i++)
+    {
+        for(j=1; j<= iCol; j++)
+        {
+           if(i==j)
+           {
+                printf("@\t",i);
+           }
+           else
+           {
+                printf("%d\t",j);
+           }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+
+    printf("enter the rows \n");
+    scanf("%d",&iValue1);
+
+    printf("enter the col \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+    
+    return 0;
+}
---
+++ program118.c
@@ -0,0 +1,66 @@
+/*
+    rows   6
+    col    6
+
+    &   &   &   &   &   &
+    &   *   *   *   *   &
+    &   *   *   *   *   &
+    &   *   *   *   *   &
+    &   *   *   *   *   &
+    &   &   &   &   &   &
+
+
+*/ 
+#include<stdio.h>
+void Display(int iRow,int iCol)
+{
+    int i=0;
+    int j=0;
+
+    if(iRow < 0)
+    {
+        iRow= -iRow;
+    }
+    if(iCol < 0)
+    {
+        iCol= -iCol;
+    }
+    if(iRow !=iCol) // jar row ani column vegvegle dile tar
+    {
+        printf("ROW number and column number are different\n");
+        return;
+    }
+
+    for(i =1; i <= iRow; i++)
+    {
+        for(j=1; j<= iCol; j++)
+        {
+           if((j == 1) || (j == iCol))
+            {
+                printf("&\t");
+            }
+            else
+            {
+                printf("*\t");
+
+            }
+           
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+
+    printf("enter the rows \n");
+    scanf("%d",&iValue1);
+
+    printf("enter the col \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+    
+    return 0;
+}
