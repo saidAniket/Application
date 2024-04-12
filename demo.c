723ba423a3b762afdd126d8a0571356fc69ec1d0
Aniket <119159622+saidAniket@users.noreply.github.com>
2024-04-12 12:11:54 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int A=10;     //integer
+
+    char ch='z';    //  character
+/*
+    printf("%d\n",A);
+
+    printf("%c\n",ch);
+
+*/
+
+    return 0;
+
+}
